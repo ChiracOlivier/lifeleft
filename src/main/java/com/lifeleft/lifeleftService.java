b4df1b2eb729270d3ca02faacca0084b8a5f76d2
Chiracolivier <chiracolivier28@gmail.com>
2019-09-17 07:16:04 +0200

    complete lifeleftService.java

--- src/main/java/com/lifeleft/lifeleftService.java
+++ src/main/java/com/lifeleft/lifeleftService.java
@@ -1,4 +1,24 @@
 package com.lifeleft;
 
+import java.time.Year;
+
 public class lifeleftService {
+
+
+    private static final Integer EXPERIENCE_VIE_FEMMES=85;
+    private static final  Integer EXPERIENCE_VIE_HOMMES=79;
+
+    String homme= "homme";
+    String femme= "femme";
+
+    Integer evRefence =0;
+
+    public String anneeRestanteAVivre(String prenom, String sexe, Integer anneeNaissance){
+        if(sexe.equals(homme)) evRefence= EXPERIENCE_VIE_HOMMES;
+        else  evRefence= EXPERIENCE_VIE_FEMMES;
+
+        Integer anneeRestante= evRefence-(Year.now().getValue()- anneeNaissance);
+
+        return "Bonjour "+ prenom+ ", il vous reste à vivre :"+ anneeRestante+" années";
+    }
 }
